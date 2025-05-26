eefa9d9eb5f59ab2c18541f4b1444c36cb547c4e
Meg Heisler <mkheisler93@gmail.com>
2025-05-26 11:51:09 -0500

    first commit

---
+++ src/main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const messageElement = document.createElement('h1');
+    messageElement.textContent = 'The Resilience Project';
+    messageElement.style.textAlign = 'center'
+    document.body.appendChild(messageElement);
+});
